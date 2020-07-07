4604c46a140e5a5b22dfcb310072f230984dc34c
Tiago Pala Baraúna <tiago_pala@outlook.com>
2020-07-07 15:54:35 -0300

    Revisando os conceitos MVC - Primeiras aulas

---
+++ DemoAspNetMvcCore/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using DemoAspNetMvcCore.Models;
+
+namespace DemoAspNetMvcCore.Controllers
+{
+    [Route("")]
+    [Route("gestao-clientes")]
+    public class HomeController : Controller
+    {
+        [Route("")]
+        [Route("pagina-inicial")]
+        [Route("pagina-inicial/{id:int}/{guid:guid}")]
+        public IActionResult Index(int id, Guid guid)
+        {
+            return View();
+        }
+
+        [Route("privacidade")]
+        [Route("politica-de-privacidade")]
+        public IActionResult Privacy(Filme filme)
+        {
+            if(ModelState.IsValid)
+            {
+
+            }
+
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        [Route("error")]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ DemoAspNetMvcCore/Models/Filme.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoAspNetMvcCore.Models
+{
+    public class Filme
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O campo Título é obrigatório")]
+        [StringLength(60, MinimumLength = 6, ErrorMessage = "O Título precisa estar entre 6 e 60 caracteres")]
+        public string Titulo { get; set; }
+
+        [Required(ErrorMessage = "O campo Data de Lançamento é obrigatório")]
+        [DataType(DataType.DateTime, ErrorMessage = "Data em formato incorreto")]
+        [Display(Name = "Data de Lançamento")]
+        public DateTime DataLancamento { get; set; }
+
+        [Required(ErrorMessage = "Preencha o campo Gênero")]
+        [StringLength(30, ErrorMessage = "Máximo de 30 caracteres")]
+        [Display(Name = "Genêro")]
+        public string Genero { get; set; }
+
+        [Required(ErrorMessage = "O campo Valor é obrigatório")]
+        [Range(1,1000, ErrorMessage = "Valor de 1 a 1000")]
+        public decimal Valor { get; set; }
+
+        [Required(ErrorMessage = "Preencha o campo Avaliação")]
+        [RegularExpression(@"^[0-5]*$", ErrorMessage = "Somente números de 0 a 5")]
+        [Display(Name = "Avaliação")]
+        public int Avaliacao { get; set; }
+    }
+}
